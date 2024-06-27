90334a8f54283535d3b4bee43471550396d7b43f
Chamsae7 <dididd0112@gmail.com>
2024-06-27 20:18:53 +0900

    동글게임
    수박게임

--- Assets/Casual Physics Puzzle BE6/Scripts/Dongle.cs
+++ Assets/Casual Physics Puzzle BE6/Scripts/Dongle.cs
@@ -9,6 +9,7 @@ public class Dongle : MonoBehaviour
     public int level;
     public bool isDrag;
     public bool isMerge;
+    public bool isAttach;
 
     public Rigidbody2D rigid;
     CircleCollider2D circle;
@@ -68,6 +69,25 @@ public class Dongle : MonoBehaviour
     {
         isDrag = false;
         rigid.simulated = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine("AttachRoutine");
+    }
+
+    IEnumerator AttachRoutine()
+    {
+        if (isAttach) {
+            yield break;
+        }
+
+        isAttach = true;
+        manager.SfxPlay(GameManager.Sfx.Attach);
+
+        yield return new WaitForSeconds(0.2f);
+
+        isAttach = false;
     }
 
     void OnCollisionStay2D(Collision2D collision)
@@ -153,6 +173,7 @@ public class Dongle : MonoBehaviour
 
         anim.SetInteger("Level", level + 1);
         EffectPlay();
+        manager.SfxPlay(GameManager.Sfx.LevelUp);
 
         yield return new WaitForSeconds(0.3f);
         level++;
